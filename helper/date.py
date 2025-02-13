8b3b6d2d0efd2a3501cadd86d6e30cc501775ad2
chutchatut <chutchatut@gmail.com>
2025-02-13 14:27:51 +0700

    update the month dict

--- helper/date.py
+++ helper/date.py
@@ -1,5 +1,32 @@
 from datetime import datetime
 
+MONTH_STR_TO_INT = {
+    "มกรา": 1,
+    "มกราคม": 1,
+    "กุมภา": 2,
+    "กุมภาพันธ์": 2,
+    "มีนา": 3,
+    "มีนาคม": 3,
+    "เมษา": 4,
+    "เมษายน": 4,
+    "พฤษภา": 5,
+    "พฤษภาคม": 5,
+    "มิถุนา": 6,
+    "มิถุนายน": 6,
+    "กรกฎา": 7,
+    "กรกฎาคม": 7,
+    "สิงหา": 8,
+    "สิงหาคม": 8,
+    "กันยา": 9,
+    "กันยายน": 9,
+    "ตุลา": 10,
+    "ตุลาคม": 10,
+    "พฤศจิกา": 11,
+    "พฤศจิกายน": 11,
+    "ธันวา": 12,
+    "ธันวาคม": 12,
+}
+
 
 def parse_date(transcription: str) -> datetime:
     parts = transcription.strip().split()
@@ -12,12 +39,6 @@ def parse_date(transcription: str) -> datetime:
     # TODO might need to convert date and year from text to int
     day = int(day)
     year = int(year)
-
-    MONTH_STR_TO_INT = {
-        "มกรา": 1,
-        "มกราคม": 1,
-        # TODO add more
-    }
 
     if month not in MONTH_STR_TO_INT:
         raise Exception('unknown month format')
